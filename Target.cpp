b44c10577030df7e88bb643e5cf39a61b92d76cf
Stellard Tom <tstellar@ix.cs.uoregon.edu>
2010-05-04 15:25:20 -0700

    Add Target and Level classes to xcode project.  Also add meshes to git.

--- Target.cpp
+++ Target.cpp
@@ -3,11 +3,11 @@
 
 Target::Target(Ogre::SceneManager *sceneManager, Ogre::Vector3 position){
 	
-	Ogre::Entity *entity = sceneManager->createEntity("Target",
-		Ogre::SceneManager::PT_PLANE);
+	Ogre::Entity *entity = sceneManager->createEntity("Target","sphere.mesh");
 	entity->setMaterialName("Target_Material");
 	sceneNode = sceneManager->getRootSceneNode()->
-				createChildSceneNode("Target", position);
+				createChildSceneNode("Targeta", position);
 	sceneNode->scale(100.0f, 100.0f, 100.0f);
 	sceneNode->attachObject(entity);
+	printf("Adding target at %lf %lf %lf\n",position.x, position.y, position.z);
 }
