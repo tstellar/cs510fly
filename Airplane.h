b53c209e80fada628b4c160bc2be8524477cc75d
Luke Maurer <Luke.Maurer@alumni.carleton.edu>
2010-05-26 00:25:49 -0700

    Be fussy about const

--- Airplane.h
+++ Airplane.h
@@ -22,7 +22,7 @@ public:
 
     float getThrust() { return thrustAmount; }
     void setThrust(float thrustAmount) { this->thrustAmount = thrustAmount; }
-    Ogre::Vector3& getPosition() { return position; }
+    const Ogre::Vector3& getPosition() const { return position; }
     const Ogre::Vector3& getVelocity() const { return velocity; }
 
     float getPitch();
