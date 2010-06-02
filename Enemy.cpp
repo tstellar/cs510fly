d27d3384a74c84237a27defc72be4eb5accebbd9
Tom Stellard <tstellar@gmail.com>
2010-06-02 15:53:41 -0700

    Refactor Enemy AI.
    Added AirplaneAI class for when the enemy is flying a plane.

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,56 +1,42 @@
 #include "Enemy.h"
 #include "Airplane.h"
+#include "AirplaneAI.h"
 
 Enemy::Enemy(Game * game, Ogre::SceneNode * sceneNode, const AirplaneState& state, Ogre::String name) :
-        game(game), airplane(new Airplane(game, sceneNode, state)) {
+        game(game), airplane(new Airplane(game, sceneNode, state)),
+        ai(new AirplaneAI(airplane)) {
     Ogre::SceneManager * sceneManager = sceneNode->getCreator();
     Ogre::Entity * entity = sceneManager->createEntity(name, "sphere.mesh");
     entity->setMaterialName("Enemy_Material");
     sceneNode->scale(25.0f, 25.0f, 25.0f);
     sceneNode->attachObject(entity);
+
 }
 
-void Enemy::update(Airplane * player, float dt) {
+/*These functions should either be static or moved elsewhere. */
+bool Enemy::playerBelow(Airplane * player, const Ogre::Vector3& pos) {
+	return player->getPosition().y < pos.y;
+}
+
+bool Enemy::playerLeft(Airplane * player, const Ogre::Vector3& pos) {
+	return player->getPosition().x < pos.x;
+}
+
+bool Enemy::playerInFront(Airplane * player, const Ogre::Vector3& pos) {
     const Ogre::Vector3& playerPosition = player->getPosition();
-    const Ogre::Vector3& enemyPosition = this->airplane->getPosition();
-    /* TODO: not rolling, not pitching, and not yawing should be an option
-     * if the enemy is within some constant amount of the player. */
-    
-    if(playerPosition.y < enemyPosition.y){
-        airplane->pitchDown();
-    }else{
-        airplane->pitchUp();
-    }
-
-    if(playerPosition.x < enemyPosition.x){
-        airplane->yawLeft();
-    }else{
-        airplane->yawRight();
-    }
-
     /* These vectors only need two components, but I am using a Vector3 here
      * because the Vector3 class has better builtin fucntions and I am lazy.
      */
     Ogre::Vector3 enemyToPlayer = Ogre::Vector3(
-                                        playerPosition.x - enemyPosition.x,
-                                        playerPosition.z - enemyPosition.z, 0);
-    Ogre::Vector3 enemyXZ = Ogre::Vector3(enemyPosition.x, enemyPosition.z, 0);
-    Ogre::Radian angleBetween = enemyToPlayer.angleBetween(enemyXZ);
+                                        playerPosition.x - pos.x,
+                                        playerPosition.z - pos.z, 0);
+    Ogre::Vector3 posXZ = Ogre::Vector3(pos.x, pos.z, 0);
+    const Ogre::Radian& angleBetween = enemyToPlayer.angleBetween(posXZ);
+    return Ogre::Math::Abs(angleBetween) < Ogre::Radian(Ogre::Math::HALF_PI);; 
+}
 
-    /* Turn around to the right. */
-    if(angleBetween > Ogre::Radian(Ogre::Math::HALF_PI)){
-        airplane->decreaseThrust();
-        airplane->rollRight();
-        airplane->yawRight();
-    }
-
-    /* Turn around to the left. */
-    if(angleBetween < Ogre::Radian(-Ogre::Math::HALF_PI)){
-        airplane->decreaseThrust();
-        airplane->rollLeft();
-        airplane->yawLeft();
-    }
-    airplane->update(dt);
+void Enemy::update(Airplane * player, float dt) {
+    ai->update(this, player, dt);
 }
 
 Enemy::~Enemy() { }
--- Enemy.h
+++ Enemy.h
@@ -2,14 +2,20 @@
 #include "AirplaneState.h"
 
 class Airplane;
+class AI;
 
 class Enemy{
     Game * const game;
     Airplane * airplane;
+    AI * ai;
 
 public:
     Enemy(Game *, Ogre::SceneNode *, const AirplaneState&, Ogre::String);
     ~Enemy();
-
+    
     void update(Airplane *, float dt);
+
+    bool playerBelow(Airplane * player, const Ogre::Vector3& pos);
+    bool playerLeft(Airplane * player, const Ogre::Vector3& pos);
+    bool playerInFront(Airplane * player, const Ogre::Vector3& pos);
 };
